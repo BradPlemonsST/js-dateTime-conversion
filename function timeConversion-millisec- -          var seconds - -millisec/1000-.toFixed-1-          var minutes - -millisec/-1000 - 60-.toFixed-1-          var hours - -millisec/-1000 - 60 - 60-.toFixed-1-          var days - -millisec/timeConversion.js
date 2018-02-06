1a6e2a86a35510103660670a654568653b8f33fb
brad-plemons <35238385+brad-plemons@users.noreply.github.com>
2018-02-06 13:10:28 -0600

    initial commit

---
+++ function timeConversion-millisec- -          var seconds - -millisec/1000-.toFixed-1-          var minutes - -millisec/-1000 - 60-.toFixed-1-          var hours - -millisec/-1000 - 60 - 60-.toFixed-1-          var days - -millisec/timeConversion.js
@@ -0,0 +1,29 @@
+ function subtractDateTime () {
+ 
+ };
+ 
+ 
+ 
+ function timeConversion(millisec) {
+  
+     // get difference between two date objects in milliseconds
+        let _millisec = Math.abs(new Date() - yourDateObject);
+
+        var seconds = (millisec / 1000).toFixed(1);
+
+        var minutes = (millisec / (1000 * 60)).toFixed(1);
+
+        var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+
+        var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+        if (seconds < 60) {
+            return seconds + " Sec";
+        } else if (minutes < 60) {
+            return minutes + " Min";
+        } else if (hours < 24) {
+            return hours + " Hrs";
+        } else {
+            return days + " Days"
+        }
+    }
